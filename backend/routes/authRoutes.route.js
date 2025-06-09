3bb70268456b9a4aacea78ed933c80f428f555f0
itz-Hiru <hirumithagithub@gmail.com>
2025-06-09 08:40:35 +0530

    Create API for upload profile image

--- backend/routes/authRoutes.route.js
+++ backend/routes/authRoutes.route.js
@@ -5,11 +5,26 @@ import {
   getUserProfile,
 } from "../controllers/authController.controller.js";
 import { protect } from "../middleware/authMiddleware.middleware.js";
+import upload from "../middleware/uploadMiddleware.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser); // Register/ Signup User
 router.post("/login", loginUser); // Login User
 router.get("/profile", protect, getUserProfile); // Get logged-in user profile
+
+router.post("/upload/image", upload.single("image"), (req, res) => {
+  // Check  if file is uploaded
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  // Create image url
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+
+  res.status(200).json({ imageUrl });
+});
 
 export default router;
