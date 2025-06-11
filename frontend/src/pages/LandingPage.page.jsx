8d0216b79459705595b5e4515fb0e9bd16025b48
itz-Hiru <hirumithagithub@gmail.com>
2025-06-11 09:26:41 +0530

    Created Landing page structure

--- frontend/src/pages/LandingPage.page.jsx
+++ frontend/src/pages/LandingPage.page.jsx
@@ -1,7 +1,53 @@
-const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar.component";
+import Modal from "../components/Modals/Modal.component";
+import Login from "./authentication/Login.page";
+import SignUp from "./authentication/SignUp.page";
+import Footer from "../components/Footer/Footer.component";
+import Hero from "../sections/Hero/Hero.section";
+import Features from "../sections/Features/Features.section";
+import TopCharts from "../sections/TopCharts/TopCharts.section";
+import Testimonials from "../sections/Testimonials/Testimonials.section";
+import CTA from "../sections/CTA/CTA.section";
 
-export default LandingPage
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {}
+  return (
+    <div>
+      <Navbar onClick={() => setOpenAuthModal(true)} />
+      <div className="relative z-10">
+        <div className="container mx-auto">
+          <Hero onClick={handleCTA} />
+          <Features />
+          <TopCharts />
+          <Testimonials />
+          <CTA />
+        </div>
+        <Footer />
+      </div>
+      <Modal
+        isOpen={openAuthModal}
+        onClose={() => {
+          setOpenAuthModal(false);
+          setCurrentPage("login");
+        }}
+        hideHeader
+      >
+        <div>
+          {currentPage === "login" && <Login setCurrentPage={setCurrentPage} />}
+          {currentPage === "signup" && (
+            <SignUp setCurrentPage={setCurrentPage} />
+          )}
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default LandingPage;
