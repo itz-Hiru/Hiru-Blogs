b2f2af4f01a4541ead2875fe51727d4f97a73f16
itz-Hiru <hirumithagithub@gmail.com>
2025-06-11 13:11:42 +0530

    Created Navbar

---
+++ frontend/src/utils/data.util.js
@@ -0,0 +1,27 @@
+export const NAV_LINKS = [
+  {
+    id: "01",
+    name: "Home",
+    path: "#",
+  },
+  {
+    id: "02",
+    name: "About",
+    path: "#about"
+  },
+  {
+    id: "03",
+    name: "Features",
+    path: "#features",
+  },
+  {
+    id: "04",
+    name: "Top Charts",
+    path: "#topcharts",
+  },
+  {
+    id: "05",
+    name: "Testimonials",
+    path: "#testimonials",
+  },
+];
