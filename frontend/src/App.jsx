ed53f573a8f7b5579fbffc8163399a0aa4544828
itz-Hiru <hirumithagithub@gmail.com>
2025-06-12 14:43:04 +0530

    Created User Context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,43 +11,46 @@ import PostByTags from "./pages/user/PostByTags.page";
 import SearchPost from "./pages/user/SearchPost.page";
 import PrivateRoute from "./routes/PrivareRoute.routes";
 import UserDashboard from "./pages/user/Dashboard.page";
+import UserProvider from "./context/userContext.context";
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          {/* Default Route */}
-          <Route path="/" element={<LandingPage />} />
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path="/" element={<LandingPage />} />
 
-          {/* User Routes */}
-          <Route path="/dashboard" element={<UserDashboard />} />
-          <Route path="/:slug" element={<BlogPostView />} />
-          <Route path="/tags/:tagName" element={<PostByTags />} />
-          <Route path="/search" element={<SearchPost />} />
+            {/* User Routes */}
+            <Route path="/dashboard" element={<UserDashboard />} />
+            <Route path="/:slug" element={<BlogPostView />} />
+            <Route path="/tags/:tagName" element={<PostByTags />} />
+            <Route path="/search" element={<SearchPost />} />
 
-          {/* Admin Routes */}
-          <Route element={<PrivateRoute allowedRoles={["Admin"]} />}>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/posts" element={<BlogPosts />} />
-            <Route
-              path="/admin/posts/edit/:postSlug"
-              element={<BlogPostEditor />}
-            />
-            <Route path="/admin/posts/create" element={<BlogPostCreator />} />
-            <Route path="/admin/comments" element={<Comments />} />
-          </Route>
-        </Routes>
-      </Router>
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            fontSize: "13px",
-          },
-        }}
-      />
-    </div>
+            {/* Admin Routes */}
+            <Route element={<PrivateRoute allowedRoles={["Admin"]} />}>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/posts" element={<BlogPosts />} />
+              <Route
+                path="/admin/posts/edit/:postSlug"
+                element={<BlogPostEditor />}
+              />
+              <Route path="/admin/posts/create" element={<BlogPostCreator />} />
+              <Route path="/admin/comments" element={<Comments />} />
+            </Route>
+          </Routes>
+        </Router>
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
+    </UserProvider>
   );
 };
 
