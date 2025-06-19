45bc7837d7ce5f48a5ca56f72c7943f1f13f96ba
itz-Hiru <hirumithagithub@gmail.com>
2025-06-19 10:36:40 +0530

    Created search post

--- frontend/src/components/Navbar/BlogNavbar.component.jsx
+++ frontend/src/components/Navbar/BlogNavbar.component.jsx
@@ -1,34 +1,60 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LuSearch } from "react-icons/lu";
 import ProfileInfoCard from "../Cards/ProfileInfoCard.component";
+import toast from "react-hot-toast";
 
 const BlogNavbar = () => {
-  const [openSearchBar, setOpenSearchBar] = useState(false);
+  const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
+
+  const handleSearch = async () => {
+    if (!query) {
+      toast.error("Please enter something to search");
+      return;
+    }
+    setQuery("");
+    navigate(`/search?query=${query}`);
+  };
+
   return (
-    <div className="bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-5 md:px-16 lg:px-24 xl:px-32">
-      <div className="flex items-center justify-between gap-5">
-        <div className="flex">
-          <Link to="/" className="cursor-default">
-            <span className="font-header text-2xl text-black text-bold">
-              Hiru Blogs
-            </span>
-          </Link>
-        </div>
-        <div className="flex items-center gap-10">
-          <button
-            type="button"
-            className="hover:text-sky-500 cursor-pointer"
-            onClick={() => setOpenSearchBar(true)}
-          >
-            <LuSearch className="text-[22px]" />
-          </button>
-          <div className="hidden md:block">
-            <ProfileInfoCard />
+    <>
+      <div className="bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-5 md:px-16 lg:px-24 xl:px-32">
+        <div className="flex items-center justify-between gap-5">
+          <div className="flex">
+            <Link to="/" className="cursor-default">
+              <span className="font-header text-2xl text-black text-bold">
+                Hiru Blogs
+              </span>
+            </Link>
+          </div>
+          <div className="flex items-center gap-10">
+            <div className="flex items-center space-x-2">
+              <div className="relative flex items-center">
+                <input
+                  type="text"
+                  value={query}
+                  onChange={({ target }) => setQuery(target.value)}
+                  placeholder="Search..."
+                  className="pl-4 pr-10 py-2 w-[200px] md:w-[250px] lg:w-[300px] border border-gray-300 rounded-lg outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-sky-500 transition duration-200"
+                />
+                <button
+                  type="button"
+                  onClick={handleSearch}
+                  className="absolute right-0 top-0 bottom-0 px-3 flex items-center justify-center text-white bg-sky-500 hover:bg-sky-600 rounded-r-lg transition duration-200"
+                >
+                  <LuSearch className="w-5 h-5" />
+                </button>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <ProfileInfoCard />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/user/SearchPost.page.jsx
+++ frontend/src/pages/user/SearchPost.page.jsx
@@ -1,7 +1,69 @@
-const SearchPost = () => {
-  return (
-    <div>SearchPost</div>
-  )
-}
+import { useNavigate, useSearchParams } from "react-router-dom";
+import BlogLayout from "../../components/Layouts/BlogLayout.component";
+import { useEffect, useState } from "react";
+import moment from "moment";
+import axiosInstance from "../../utils/axiosInstance.util";
+import { API_PATHS } from "../../utils/apiPaths.util";
+import BlogPostCard from "../../components/Cards/BlogPostCard.component";
 
-export default SearchPost
+const SearchPost = () => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.POST.SEARCH_POST, {
+        params: { q: query },
+      });
+      if (response.data) {
+        setSearchResults(response.data || []);
+      }
+    } catch (error) {
+      console.error("Error fetching results: ", error);
+    }
+  };
+
+  const handleClick = (post) => {
+    navigate(`/${post.slug}`);
+  };
+
+  useEffect(() => {
+    handleSearch();
+    return () => {};
+  }, [query]);
+  return (
+    <BlogLayout>
+      <div>
+        <h3 className="text-lg font-medium">
+          Showing search results matching "
+          <span className="font-semibold">{query}</span>"
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
+          {searchResults.length > 0 &&
+            searchResults.map((item) => (
+              <BlogPostCard
+                key={item._id}
+                title={item.title}
+                coverImageUrl={item.coverImageUrl}
+                content={item.content}
+                tags={item.tags}
+                updatedAt={
+                  item.updatedAt
+                    ? moment(item.updatedAt).format("Do MMM YYYY")
+                    : "-"
+                }
+                authorName={item.author.name}
+                authorProfilePicture={item.author.profileImageUrl}
+                onClick={() => handleClick(item)}
+              />
+            ))}
+        </div>
+      </div>
+    </BlogLayout>
+  );
+};
+
+export default SearchPost;
